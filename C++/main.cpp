54ee17c0048299427188e2c129a93adc11a01f89
Patrick Saperstein <patricksaperstein@gmail.com>
2014-06-05 09:53:53 -0600

    updated Main for testing

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,22 @@
 #include "Song.h"
+#include "BorrowedSong.h"
 #include <cstdlib>
 #include <iostream>
 
 using namespace std;
+void testSong();
+void testBorrowedSong();
 
 int main(int argc, char *argv[])
 {
+    cout<<"Test Borrowed Song"<<endl;
+    testBorrowedSong();
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
+
+
+void testSong(){
     string title="Don't Stop Believin";
     string artist="Journey";
     string album="Escape";
@@ -17,6 +28,19 @@ int main(int argc, char *argv[])
     Song a(title, artist, album, year, composer, genre, owner, songid);
     a.play();
     cout<<"Song is: "<<a.toString()<<endl;
-    system("PAUSE");
-    return EXIT_SUCCESS;
+}
+
+void testBorrowedSong(){
+    string title="Don't Stop Believin";
+    string artist="Journey";
+    string album="Escape";
+    int year=1981;
+    string composer="Jonathan Cain";
+    string genre="Rock";
+    string owner="Psap";
+    string songid="Psap01";
+    string borrower="Jenny";
+    BorrowedSong a(title, artist, album, year, composer, genre, owner, songid, borrower);
+    a.play();
+    cout<<"Borrowed Song is: "<<a.toString()<<endl;
 }
